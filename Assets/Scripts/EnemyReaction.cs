707532707c64b8f2fabe7d2e67aa128be892beda
Daniel Atkinson <hourd.tasa@gmail.com>
2020-12-28 20:19:14 +0000

    use scene manager to reliably change scenes

--- Assets/Scripts/EnemyReaction.cs
+++ Assets/Scripts/EnemyReaction.cs
@@ -6,19 +6,25 @@ using UnityEngine.SceneManagement;
 public class EnemyReaction : MonoBehaviour
 {
     public static int sceneChange = 0;
+    public static Scene scene;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Scene: " + State.currentScene);
+        // Debug.Log("Scene: " + State.currentScene);
+        // scene = SceneManager.GetActiveScene();
+        // Debug.Log("Active Scene is '" + scene.name + "'.");
     }
 
     // Update is called once per frame
     void Update()
     {
         if(sceneChange != 0) {
-            State.currentScene += sceneChange;
-            sceneChange = 0;
-            SceneManager.LoadScene (State.scenes[State.currentScene]);
+            // State.currentScene += sceneChange;
+            // sceneChange = 0;
+            // Debug.Log("Scenes: " + State.scenes.Length);
+            // Debug.Log("Attempting to switch to scene ID: " + State.currentScene);
+            // Debug.Log("Attempting to switch to scene Name: " + State.scenes[State.currentScene]);
+            // SceneManager.LoadScene (State.scenes[State.currentScene]);
         }
     }
 
@@ -38,13 +44,14 @@ public class EnemyReaction : MonoBehaviour
         if (other.CompareTag ("Finish")) {
             Debug.Log("Scene Finish: " + State.currentScene);
             State.savedScore = State.ScoreValue;
-            // State.nextScene();
-            sceneChange = State.currentScene + 1;
+            State.nextScene();
+            // sceneChange = State.currentScene + 1;
         }
         if (other.CompareTag ("Win")) {
             Debug.Log("Win");
             State.savedScore = State.ScoreValue;
-            SceneManager.LoadScene ("Win");
+            // SceneManager.LoadScene ("Win");
+            State.winScene();
         }
     }
 }
--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -8,7 +8,8 @@ public class State : MonoBehaviour
     public static List<string> items = new List<string>();
     public static int ScoreValue = 0;
     public static int Lives = 5;
-    public static string[] scenes = {"Scene1", "Scene2", "Scene3", "Win", "Lose"};
+    // public static string[] scenes = {"Scene1", "Scene2", "Scene3"};
+    public static List<string> scenes = new List<string> {"Scene1", "Scene2", "Scene3"};
     public static int currentScene = 0;
     public static int newSceneID = 0;
     public static int savedScore = 0;
@@ -20,11 +21,16 @@ public class State : MonoBehaviour
     void Update()
     {}
 
-    // public static void nextScene() {
-    //     newSceneID = currentScene + 1;
-    //     SceneManager.LoadScene (State.scenes[newSceneID]);
-    //     currentScene = newSceneID;
-    // }
+    public static void nextScene() {
+        Debug.Log("State loading next scene");
+        Scene scene = SceneManager.GetActiveScene();
+        int index = scenes.IndexOf(scene.name);
+        Debug.Log("Current scene index: " + index);
+        currentScene = index + 1;
+        Debug.Log("Next scene index: " + currentScene);
+        Debug.Log("Next scene name: " + scenes[currentScene]);
+        SceneManager.LoadScene (scenes[index + 1]);
+    }
 
     // public static void previousScene() {
     //     newSceneID = currentScene - 1;
@@ -33,6 +39,11 @@ public class State : MonoBehaviour
     // }
 
     public static void loseScene() {
-        SceneManager.LoadScene (State.scenes[State.scenes.Length - 1]);
+        // SceneManager.LoadScene (State.scenes[State.scenes.Length - 1]);
+        SceneManager.LoadScene ("Lose");
+    }
+    public static void winScene() {
+        // SceneManager.LoadScene (State.scenes[State.scenes.Length - 2]);
+        SceneManager.LoadScene ("Win");
     }
 }
